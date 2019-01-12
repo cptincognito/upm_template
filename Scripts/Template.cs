563b78b171a01275171709a38180a468ad93bdfd
Evan Harper <harper4@gmail.com>
2019-01-12 15:35:13 -0500

    Test commit

--- Scripts/Template.cs
+++ Scripts/Template.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Template : MonoBehaviour
 {
+    public string myString = "Test";
     // Start is called before the first frame update
     void Start()
     {
-        
+       Debug.Log(myString);
     }
 
     // Update is called once per frame
